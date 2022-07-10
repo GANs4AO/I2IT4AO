8482a849481d21b6d4d51892f3031f867784b4df
Jeffrey Smith <jeffrey.smith@anu.edu.au>
2022-07-10 18:30:38 +1000

    adding supporting files

---
+++ Roket_batch.py
@@ -0,0 +1,16 @@
+import os
+from tqdm import tqdm
+import subprocess
+
+directory = '/home/jeff/GITs/COMPASS/r0_batch/'
+directory = './COMPASS_PARAM/r0_batch2/'
+for filename in os.listdir(directory):
+    if filename.endswith(".py"):
+        print(os.path.join(directory, filename))
+        #os.system("python3 /home/jeff/shesha/guardians/roket.py /home/jeff/GITs/COMPASS/r0_batch/r0_35_W10_p512_s9999.py")# + os.path.join(directory, filename))
+        os.system('python3 /home/jeff/shesha/guardians/roket.py ' + os.path.join(directory, filename))
+        os.rename('/home/jeff/roket_output/roket_default.h5', '/home/jeff/roket_output/batch/roket_' + filename + '.h5')
+
+    else:
+        continue
+
